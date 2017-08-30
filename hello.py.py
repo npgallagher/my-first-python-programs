ccf6d0086df07bd84269796fd275220c1ccf8a98
npgallagher <31477553+npgallagher@users.noreply.github.com>
2017-08-30 09:12:03 -0400

    Add files via upload

---
+++ hello.py.py
@@ -0,0 +1,16 @@
+#This program says hello and greets a person by name.
+#
+#Your Name
+#August 24, 2017
+
+print("Hello.")
+print("What is your name?")
+name = input()
+print("It is good to meet you, " + name + ".")
+print("Where were you born, " + name + "?")
+location = input()
+print("That's interesting. I'd like to visit " + location + " one day.")
+print("What's your favorite color?")
+color = input()
+print("Wow, " + color + " is my favorite color too!")
+input()
